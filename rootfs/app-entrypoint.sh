33a9d90a23f9d7b61ca1dfcb8499e008c5a5b4cd
urgemerge <urgemerge@users.noreply.github.com>
2018-08-03 17:09:10 +0200

    Update app-entrypoint.sh

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -4,6 +4,7 @@
 . /opt/bitnami/base/helpers
 
 print_welcome_page
+echo "172.20.100.87 gitlab.buw.local" >> /etc/hosts
 cd /app ; rm -Rf * ; git clone http://gitlab.buw.local/vandij-039/shiny-webroot.git . ; cd -
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
