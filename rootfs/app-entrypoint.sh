70386705e747ab4d79512cdbfcefad6f172220b1
urgemerge <urgemerge@users.noreply.github.com>
2018-08-07 14:30:30 +0200

    Update app-entrypoint.sh

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -5,8 +5,7 @@
 
 print_welcome_page
 
-cd /tmp ; wget -r -np -k http://172.20.100.87:1337 ; cp -R 172.20.100.87:1337/* /app
-#cd /app ; rm -Rf * ; git clone http://gitlab.buw.local/vandij-039/shiny-webroot.git . ; cd -
+cd /tmp ; wget http://172.20.100.87:1337/slides.tar.gz ; tar -zxvf slides.tar.gz; cp -Rv * /app
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/run.sh" ]]; then
   nami_initialize apache
